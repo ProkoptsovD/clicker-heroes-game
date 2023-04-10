5115e21720e89020121f35ef9fc4aea1c1a3621f
ProkoptsovD <fenderman1992@gmail.com>
2023-04-10 16:03:12 +0300

    feat: Add enemy base class

--- index.js
+++ index.js
@@ -11,24 +11,32 @@ import { menuPreviews } from './src/constants/menuPreviews.js';
 
 import { store } from './src/store/store.js';
 import { SignUpForm } from './src/components/SignUpForm.js';
+import { Enemy } from './src/lib/Enemy.js';
 
 const rootContainer = document.querySelector('#root');
+const enemy = new Enemy({
+  phrases: {
+    intro: "Hi, Ventura! You think you can get me? Let's try!!!",
+    isArrested: "Noooooooo! I can't believe you have caught me...."
+  }
+});
 
 function initApp() {
-  rootContainer.appendChild(new SignUpForm());
-  rootContainer.appendChild(
-    new HomeScreen({
-      childrenElements: [
-        {
-          tag: GameMenu.tag,
-          props: {
-            items: ['Story', 'Settings', 'Credits', 'Quit'],
-            previews: menuPreviews
-          }
-        }
-      ]
-    })
-  );
+  enemy.addToDOM(rootContainer);
+  // rootContainer.appendChild(new SignUpForm());
+  // rootContainer.appendChild(
+  //   new HomeScreen({
+  //     childrenElements: [
+  //       {
+  //         tag: GameMenu.tag,
+  //         props: {
+  //           items: ['Story', 'Settings', 'Credits', 'Quit'],
+  //           previews: menuPreviews
+  //         }
+  //       }
+  //     ]
+  //   })
+  // );
 }
 
 initApp();
